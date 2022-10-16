a588e0e7576ee556f98923a4082674f12d77815c
David Fagan <david.fagan@diona.com>
2022-10-16 11:07:57 +0100

    Added logging

--- app/src/main/java/org/wit/foraging/ForagingActivity.kt
+++ app/src/main/java/org/wit/foraging/ForagingActivity.kt
@@ -2,10 +2,16 @@ package org.wit.foraging
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import timber.log.Timber
+import timber.log.Timber.i
 
 class ForagingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_foraging)
+        Timber.plant(Timber.DebugTree())
+        i("Foraging Activity started..")
+
+
     }
 }
